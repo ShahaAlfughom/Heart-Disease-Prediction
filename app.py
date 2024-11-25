3a61058e7dcb9d33280ae696ca84b08734c4c06d
Shaha Alfughom <shahaalfughom@Shahas-MacBook-Air.local>
2024-11-25 22:00:13 +0300

    last ubdate

--- app.py
+++ app.py
@@ -64,5 +64,8 @@ def predict():
     except Exception as e:
         return render_template('result.html', prediction_text=f"Error: {str(e)}")
 
+#if __name__ == '__main__':
+      # app.run(debug=True)
+
 if __name__ == '__main__':
-       app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
