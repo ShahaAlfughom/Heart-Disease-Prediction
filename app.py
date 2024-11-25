5afc6b1412866e790ffb6cfdbd87ea8da71909a8
Shaha Alfughom <shahaalfughom@Shahas-MacBook-Air.local>
2024-11-25 21:54:14 +0300

    udate

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def predict():
     except Exception as e:
         return render_template('result.html', prediction_text=f"Error: {str(e)}")
 
-#if __name__ == '__main__':
-       # app.run(debug=True)
+if __name__ == '__main__':
+       app.run(debug=True)
